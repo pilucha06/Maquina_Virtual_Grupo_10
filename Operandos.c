9cac6d5d6ca9a5dac26265527435ef44ecb0a9df
tiziana <tizianac0506@gmail.com>
2026-09-14 00:18:56 -0300

    estructura terminada y checkeada

--- Operandos.c
+++ Operandos.c
@@ -25,15 +25,63 @@ es registro, memoria o inmediato cada vez que se ejecuta una instruccion.
   ->Escalabilidad y legibilidad: El codigo principal (get y set) queda resuelto 
 en una sola linea muy limpia.
   ->Rendimiento: El acceso mediante indice tabla[tipo](...)
-se resuelve en tiempo $O(1)$, ejecutandose directo en el hardware.*/
-
+se resuelve en tiempo O(1), ejecutandose directo en el hardware.*/
+ 
 #include <stdio.h>
 #include "Operandos.h"
+#include "Ejecucion.h"
 
-//MIS CONSTANTES 
 #define MAXVEC 4
 
-//GETTERS!
+//FUNCIONES PARTICULARES DE MEMORIA 
+//cito de la especificación:
+/*Cada vez que se realiza una operación en la memoria, se debe cargar en el registro LAR la dirección
+lógica a la que se quiere acceder y la cantidad de bytes en la parte alta del registro MAR (los 2 bytes más
+significativos). Luego de realizar la traducción a una dirección física, el resultado debe almacenarse en la
+parte baja del registro MAR (los 2 bytes menos significativos). En el registro MBR debe quedar el valor con
+el cual se está operando, ya sea el valor que se desea almacenar en el caso de una escritura o el que se
+obtuvo después de la lectura. La lectura de la instrucción no debe modificar ninguno de estos registros*/
+
+void leerMemoria(tRegMV *mv, int direLogica, int *valor){
+    int i, direFisica;
+    direFisica = dire_fisica(direLogica, mv, 4);
+    if (direFisica == -1 ){
+        printf("Error: fallo de segmento al leer memoria.\n");
+    }
+    else{
+        //LAR = REG[4] y MAR = REG[5]
+        mv->registros[4] = direLogica;
+        mv->registros[5] = (4<<16) | (direFisica & 0xFFFF);
+        //lectura
+        *valor = 0; //x las dudas
+        for(i=0; i<4; i++) 
+            //voy concatenando 4 veces en "valor" los 8 bits de cada celda en RAM
+            *valor = (*valor<<8) | mv->RAM[direFisica + i];
+        //MBR = REG[6]
+        mv->registros[6] = *valor;
+    }
+}
+
+void escribirMemoria(tRegMV *mv, int direLogica, int valor){
+    int direFisica;
+    direFisica = dire_fisica(direLogica, mv, 4);
+    if (direFisica == -1 ){
+        printf("Error: fallo de segmento al escribir en memoria.\n");
+    }
+    else{
+        //LAR = REG[4] y MAR = REG[5] y MBR = REG[6]
+        mv->registros[4] = direLogica;
+        mv->registros[5] = (4<<16) | (direFisica & 0xFFFF);
+        mv->registros[6] = valor;
+        //escribo 4  bytes en RAM
+        mv->RAM[direFisica + 0] = (valor >> 24) & 0xFF;
+        mv->RAM[direFisica + 1] = (valor >> 16) & 0xFF;
+        mv->RAM[direFisica + 2] = (valor >> 8)  & 0xFF;
+        mv->RAM[direFisica + 3] = valor & 0xFF; 
+    }
+}
+
+//GETTERS
 
 int getReg(tRegMV *mv, int op){
     int regNum ;
@@ -46,30 +94,44 @@ int getInm(tRegMV *mv, int op){
 }
 
 int getMem(tRegMV *mv, int op){
-    int regNum, segmento, direLogica, direFisica;
-    regNum = op & 0x1F // dire->5 ult bits
+    int regNum, segmento, direLogica, offset, valor;
+    regNum = op & 0x1F; // dire->5 ult bits
     offset = (op >> 8) &  0xFFFF; // 8=dire+3 reservados
     segmento= mv->registros[regNum];
     //calculo direccion logica = 2 bytes +s segmento + 2 bytes -s offset
     direLogica = (segmento << 16) | (offset & 0xFFFF);
-        direFisica = sacoDireFisica(mv, direLogica);
-        return leerMemoria(mv, direFisica); // las hace martu *n* ... (en ejecucion.c)
+    valor = 0;
+    leerMemoria(mv, direLogica, &valor);
+    return valor; 
 }           
 
+//SETTERS
+void setReg(tRegMV *mv, int op, int valor){
+    int regNum = op & 0x1F; //5 ult bits -> codreg
+    mv->registros[regNum] = valor;
+}
 
-void setReg(tRegMv *mv, int op, int valor){
-    int regNum;
-    regNum = op & 0x1F;
-    mv->registros[regNum] = valor; 
+void setMem(tRegMV *mv, int op, int valor){
+    int regNum, segmento, direLogica, offset;
+    regNum = op & 0x1F; // dire->5 ult bits
+    offset = (op >> 8) &  0xFFFF; // 8=dire+3 reservados
+    segmento= mv->registros[regNum];
+    //calculo direccion logica = 2 bytes +s segmento + 2 bytes -s offset
+    direLogica = (segmento << 16) | (offset & 0xFFFF);
+    escribirMemoria(mv, direLogica, valor);
+}        
 
+void setInm(tRegMV *mv, int op, int valor) {
+    printf("Error: Operando inmediato no escribible.\n");
+}
 
-}            
+//FUNCIONES PPALES Y DEFINICIONES
 
 typedef int (*getters)(tRegMV *, int);
 typedef void (*setters)(tRegMV *, int, int);
 
-getters vecGet[MAXVEC] = { NULL, getReg, getInm, getMem}
-setters setVec[MAXVEC] = { NULL, setReg, setInm, setMem }
+getters vecGet[MAXVEC] = { NULL, getReg, getInm, getMem };
+setters vecSet[MAXVEC] = { NULL, setReg, setInm, setMem };
 
 
 //en la llamada, debemos referenciar:  OP1-> REG[2] y OP2-> REG[3]
@@ -77,15 +139,15 @@ int get(tRegMV *mv, int op){
    int tipo;
    tipo=(op >> 24) & 0xFF; 
    if (tipo >=1 && tipo<=3 && vecGet[tipo] != NULL) 
-        return tablaGet[tipo](mv, op);
+        return vecGet[tipo](mv, op);
     else
         return 0; //?
 }
 
 void set(tRegMV *mv, int op, int valor){
     int tipo;
-    tipo = (op >> 24) && OXFF;
-    if (tipo>=1 && tipo <=3 && vecSet[tipo != NULL])
-        vecSet[tipo](mv, op, valor)
+    tipo = (op >> 24) & 0XFF;
+    if (tipo>=1 && tipo <=3 && vecSet[tipo] != NULL)
+        vecSet[tipo](mv, op, valor);
 }
 
