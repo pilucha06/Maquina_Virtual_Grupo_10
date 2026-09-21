068cbaa93d8f96fe78a4a7e562bd2576afa976a8
Tiziana Castello <135176979+tizolin@users.noreply.github.com>
2026-09-21 16:18:49 -0300

    instrucciones terminadas

--- Instrucciones.c
+++ Instrucciones.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include "Instrucciones.h"
 
+
 instrucciones vecInstr[MAXINSTR] = { SYS, JMP, JP, JN, JZ, JC, JV, JNP, JNN, JNZ, NOT, NULL, NULL, NULL, NULL, STOP, MOV, ADD, SUB, MUL, DIV, CMP, AND, OR, XOR, SWAP, SHL, SHR, SAR, LDL, LDH, RND };
 
 //llamado a sistema
@@ -13,8 +14,7 @@ void SYS(TMV *mv, int opa, int opb){
 }
 
 //-------SECCION JUMPS--------
-//ORDEN DE BITS CC:
-//N Z C V
+// BITS CC (4 bits más significativos): [N] [Z] [C] [V]
 
 void salto(TMV *mv, int opa){
     int inicioCS, opA;
@@ -29,7 +29,7 @@ void JMP(TMV *mv, int opa, int opb){ //salta siempre
 }
 
 void JP(TMV *mv, int opa, int opb){ //salta cuando es positivo
-    if (!((mv->registros[REG_CC] >> 28) & 0x12)) //masc=1100 tiene q dar 0
+    if (!((mv->registros[REG_CC] >> 28) & 0xC)) //masc=1100 tiene q dar 0
         salto(mv, opa);
 }
 
@@ -54,7 +54,7 @@ void JV(TMV *mv, int opa, int opb){ //salta cuando hay overflow
 }
 
 void JNP(TMV *mv, int opa, int opb){ //salta cuando es negativo o cero
-    if ((mv->registros[REG_CC] >> 28) & 0x12) //masc=1100 tiene q dar 1
+    if ((mv->registros[REG_CC] >> 28) & 0xC) //masc=1100 tiene q dar 1
         salto(mv, opa);
 }
 
@@ -83,7 +83,7 @@ void NOT(TMV *mv, int opa, int opb){ //solamente invertir bits y actualiza reg C
 }
 
 void STOP(TMV *mv, int opa, int opb){
-    mv->registros[REG_IP]==-1;
+    mv->registros[REG_IP]=-1;
 }
 
 void MOV(TMV *mv, int opa, int opb){
@@ -126,17 +126,16 @@ void DIV(TMV *mv, int opa, int opb){
     unsigned int res;
     opA=get(mv, opa);
     opB=get(mv, opb);
-    if (!opB)
-        res = opA*opB;
+    if (opB != 0){
+        res = opA/opB;
+        set(mv, opa, res);
+        modCC(mv, res); }
     else
         mv->error=2; //div x cero
-    set(mv, opa, res);
-    modCC(mv, res); 
 }
 
 void CMP(TMV *mv, int opa, int opb){
-    int opA, opB;
-    unsigned int res;
+    int opA, opB, res;
     opA=get(mv, opa);
     opB=get(mv, opb);
     res = opA-opB;
@@ -147,8 +146,7 @@ void CMP(TMV *mv, int opa, int opb){
 registro CC. El resultado se almacena en el primer operando.*/
 
 void AND(TMV *mv, int opa, int opb){
-    int opA, opB;
-    unsigned int res;
+    int opA, opB, res;
     opA=get(mv, opa);
     opB=get(mv, opb);
     res = opA & opB;
@@ -157,8 +155,7 @@ void AND(TMV *mv, int opa, int opb){
 }
 
 void OR(TMV *mv, int opa, int opb){
-    int opA, opB;
-    unsigned int res;
+    int opA, opB, res;
     opA=get(mv, opa);
     opB=get(mv, opb);
     res = opA | opB;
@@ -168,7 +165,7 @@ void OR(TMV *mv, int opa, int opb){
 
 void XOR(TMV *mv, int opa, int opb){
     int opA, opB;
-    unsigned int res;
+    int res;
     opA=get(mv, opa);
     opB=get(mv, opb);
     res = opA ^ opB;
@@ -183,49 +180,89 @@ XOR OPN_B, OPN_A
 XOR OPN_A, OPN_B
 Por lo tanto, afecta al registro CC del mismo modo que el último XOR.*/
 
-void SWAP(){
-
+void SWAP(TMV *mv, int opa, int opb){
+    XOR(mv, opa, opb);
+    XOR(mv, opb, opa);
+    XOR(mv, opa, opb);
 }
 
 /*SHL, SHR, SAR: realizan desplazamientos de los bits almacenados en un registro o una posición de
 memoria y afectan al registro CC. SHL y SHR efectuan corrimientos a la izquierda y a la derecha
-(respectivamente) y los bits que quedan libres se completan con ceros. SAR también desplaza a la
+(respectivamente) y los bits que quedan libres se completan con ceros. */
+
+
+void SHL(TMV *mv, int opa, int opb){
+    int opA, opB, res;
+    opA=get(mv, opa);
+    opB=get(mv, opb);
+    res=opA;
+    res=res << opB;
+    set(mv, opa, res);
+    modCC(mv, res);
+}
+
+void SHR(TMV *mv, int opa, int opb){
+    int opA, opB, res;
+    opA=get(mv, opa);
+    opB=get(mv, opb);
+    res=opA;
+    res = (unsigned int)res >> opB;
+    set(mv, opa, res);
+    modCC(mv, res);
+}
+
+/*SAR también desplaza a la
 derecha, pero los bits de la izquierda propagan el bit anterior. Es decir, si el contenido es un número
 negativo, el resultado también lo será, porque agrega unos. Si es un número positivo, agrega ceros.*/
 
-
-void SHL(){
-
-}
-
-void SHR(){
-
-}
-
-void SAR(){
-
+//ni idea como programar esto por los 1s a la izq
+void SAR(TMV *mv, int opa, int opb){
+    int opA, opB, res;
+    opA=get(mv, opa);
+    opB=get(mv, opb);
+    res=opA >> opB;
+    set(mv, opa, res);
+    modCC(mv, res); 
 }
 
 /*LDL: carga los 2 bytes menos significativos del primer operando, con los 2 bytes menos significativos
 del segundo operando. Esta instrucción está especialmente pensada para poder cargar un inmediato de
 16 bits, aunque también se puede utilizar con otro tipo de operando.*/
 
-void LDL(){
-
+void LDL(TMV *mv, int opa, int opb){
+    int opA, opB, res;
+    opA=get(mv, opa);
+    opB=get(mv, opb);
+    res = (opA & 0xFFFF0000) | (opB & 0xFFFF);
+    //no modifica cc
+    set(mv, opa, res);
 }
 
 /*LDH: carga los 2 bytes más significativos del primer operando, con los 2 bytes menos significativos del
 segundo operando. Esta instrucción está especialmente pensada para poder cargar un inmediato de 16
 bits, aunque también se puede utilizar con otro tipo de operando.*/
 
-void LDH(){
-
+void LDH(TMV *mv, int opa, int opb){
+    int opA, opB, res;
+    opA=get(mv, opa);
+    opB=get(mv, opb);
+    res = ((opB & 0xFFFF) << 16) | (opA & 0x0000FFFF);
+    //no modifica cc
+    set(mv, opa, res);
 }
 
 /*RND: carga en el primer operando un número aleatorio entre 0 y el valor del segundo operando*/
 
-void RND(){
-
+//random esta en stdlib!
+void RND(TMV *mv, int opa, int opb){
+    int opB, aleatorio;
+    opB=get(mv,opb);
+    if (opB > 0){
+        aleatorio=rand() % (opB + 1);
+        set(mv, opa, aleatorio);
+    } else
+        mv->error=2; // o podemos guardar 0 :)
+    // no modifica CC
 }
 
 
